45aa0d2c4d2dd6aebde73bc687be129d30a9a104
Bill Buff <bbuff27@gmail.com>
2018-05-26 13:45:28 -0600

    Update controllers

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,16 +1,60 @@
 class WikisController < ApplicationController
+  
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @wikis = Wiki.all
   end
 
   def show
+    @wiki = Wiki.find(params[:id])
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
   end
 
   def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+    @wiki.user = current_user
+
+    if @wiki.save
+      flash[:notice] = "Your new wiki has been saved"
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "Something went wrong. Please try again"
+      render :new
+    end
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+
+    if @wiki.save
+      flash[:notice] = "Your wiki has been updated"
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "Something went wrong. Please try again"
+      render :edit
+    end
   end
 
   def destroy
+    @wiki = Wiki.find(params[:id])
+    if @wiki.destroy
+      flash[:notice] = "Your wiki has been updated"
+      redirect_to wikis_path
+    else
+      flash.now[:alert] = "Something went wrong. Please try again"
+      render :show
+    end
   end
 end
